62aa1590649d2c1be5a99a697e4bee2222c56faa
CF Grugan <charles_grugan@cable.comcast.com>
2026-02-23 22:38:38 -0500

    Initial Commit

---
+++ Salty Dog Mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Salty Dog Mobile
+//
+//  Created by Grugan, Charles on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
